3cd64c846f58cde04adc7ecb35acf9872236b55d
Your Name <lucas.barreto@leolearning.com>
2025-03-03 12:03:19 -0300

    Adding README

---
+++ lang/pt_br/tool_backup_cleanup.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language pack for tool_backup_cleanup
+ *
+ * @package    tool_backup_cleanup
+ * @copyright  2025 Lucas Barreto <lucas.b.fisica@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Limpeza de backups manuais';
+$string['task:cleanup_task'] = 'Tarefa de limpeza de backups manuais';
+$string['settings:title'] = 'Configurações de limpeza de backups manuais';
+$string['settings:enabled'] = 'Habilitar';
+$string['settings:enabled_desc'] = '';
+$string['settings:course_backups_ttl'] = 'Remover backups de cursos mais antigos que';
+$string['settings:activity_backups_ttl'] = 'Remover backups de atividades mais antigos que';
+$string['settings:private_backups_ttl'] = 'Remover backups privados mais antigos que';
